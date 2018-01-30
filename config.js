cfa1df7a859d3f9914ebdddf732a0688ca15f3f0
giangvo1125 <giangvo1125@gmail.com>
2018-01-30 17:55:26 +0700

    update

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ var config = {
 		], 
 	},
 	production: {
-		port: 3000,
-		domain: '128.199.169.165',
+		port: 3049,
+		domain: 'localhost',
 		myKey: 'B3JSHBBY-2IVG9VUI-LTWJ20K9-820I953A', 
 		mySecret: '15785c0e93066f9d8f5cb7d08c6a5e0c584f3abb77c0d01f2abcbe63df730a9616811ea449529a8a50778fd100cb3aa25aeb6eadd578990964a14b382da684ab', 
 		usdt: [
