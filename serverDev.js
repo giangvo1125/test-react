467a25f46b57f7ecae13acc972744d3414227cd6
giangvo1125 <giangvo1125@gmail.com>
2018-02-01 04:05:24 +0700

    update

--- serverDev.js
+++ serverDev.js
@@ -4,21 +4,19 @@ import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackConfigDev from './webpack.config.dev'
 import webpackConfigProd from './webpack.config.prod'
-import {domain, port} from './config'
-
+import {domain, port, scriptCSS, scriptJS} from './config'
 const app = express();
 
 const env = process.env.NODE_ENV || 'development'
-
-if(env == 'development' || env == 'test') {
-	app.use(webpackDevMiddleware(webpack(webpackConfigDev), {
-	  publicPath: webpackConfigDev.output.publicPath,
+if(env == 'production'){
+	app.use(webpackDevMiddleware(webpack(webpackConfigProd), {
+	  publicPath: webpackConfigProd.output.publicPath,
 	  stats: { colors: true }
 	}));
 }
-else if(env == 'production' || env == 'qa' || env=='demo'){
-	app.use(webpackDevMiddleware(webpack(webpackConfigProd), {
-	  publicPath: webpackConfigProd.output.publicPath,
+else {
+	app.use(webpackDevMiddleware(webpack(webpackConfigDev), {
+	  publicPath: webpackConfigDev.output.publicPath,
 	  stats: { colors: true }
 	}));
 }
@@ -28,18 +26,21 @@ app.use('/public', express.static(__dirname + '/public'))
 app.use('/assets', express.static(__dirname + '/assets'))
 
 
-app.use(express.static('assets'));
+app.use(express.static('public'));
 
+
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
+
+// app.get('*', function(req, res) {
+//   res.render('index', {scriptCSS, scriptJS});
+// });
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-var http = require('http').Server(app);
-
-console.log('process.env.PORT ',process.env.PORT)
-
-http.listen(process.env.PORT || port, domain, function (err) {
+app.listen(process.env.PORT || port, function (err) {
   if (err) {
     console.log(err);
     return;
